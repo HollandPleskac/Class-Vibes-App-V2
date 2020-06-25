9c0eea35ae86e158e5e3e2098085e37caa8b1d06
HollandPleskac <hollandpleskac@gmail.com>
2020-06-24 22:10:51 -0700

    setting up main dart with testing scaffold

---
+++ lib/testing.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Testing extends StatefulWidget {
+  @override
+  _TestingState createState() => _TestingState();
+}
+
+class _TestingState extends State<Testing> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("TESTING"),
+      ),
+    );
+  }
+}
