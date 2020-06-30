b990d0d92aa84dc9ed749f8440ee0255b5cbfec6
HollandPleskac <hollandpleskac@gmail.com>
2020-06-29 20:31:01 -0700

    new primary color

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 // Colors
 
-const kPrimaryColor = Color(0xFF3382CC);
+
+const kPrimaryColor = Color.fromRGBO(41, 128, 185,1.0); //Peter River
+// const kWetAsphalt = Color.fromRGBO(52, 73, 94,1.0);
 
 // Text Style
 const kHeadingTextStyle = TextStyle();
