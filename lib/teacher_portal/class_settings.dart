0c3f5b37613c3d504b5ba9c1b201282e51b697d4
HollandPleskac <hollandpleskac@gmail.com>
2020-07-05 12:21:30 -0700

    teacher edit class settings

--- lib/teacher_portal/class_settings.dart
+++ lib/teacher_portal/class_settings.dart
@@ -1,6 +1,9 @@
-import 'package:class_vibes_v2/constant.dart';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../constant.dart';
 
 class ClassSettings extends StatefulWidget {
   static const routeName = 'individual-class-settings-teacher';
@@ -9,17 +12,105 @@ class ClassSettings extends StatefulWidget {
 }
 
 class _ClassSettingsState extends State<ClassSettings> {
+  final TextEditingController _classNameController = TextEditingController();
+
+  var isSwitched = false;
   @override
   Widget build(BuildContext context) {
     final routeArguments = ModalRoute.of(context).settings.arguments as Map;
     final String className = routeArguments['class name'];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(className),
         centerTitle: true,
         backgroundColor: kWetAsphaltColor,
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            EditClassName(
+              controller: _classNameController,
+            ),
+            IsAcceptingJoin(isSwitched),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EditClassName extends StatelessWidget {
+  final TextEditingController controller;
+
+  EditClassName({this.controller});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 20,
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.75,
+          child: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              labelText: 'Class Name',
+
+              labelStyle: TextStyle(fontSize: 16),
+              // border: OutlineInputBorder(
+              //   borderSide: BorderSide(width: 80),
+              //   borderRadius: BorderRadius.circular(5),
+              // ),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 25,
+        ),
+        ClipOval(
+          child: Material(
+            color: Colors.white, // button color
+            child: InkWell(
+              splashColor: kPrimaryColor, // inkwell color
+              child: SizedBox(
+                width: 50,
+                height: 50,
+                child: Center(
+                  child: FaIcon(
+                    FontAwesomeIcons.check,
+                  ),
+                ),
+              ),
+              onTap: () {
+                print('updated the class name');
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class IsAcceptingJoin extends StatefulWidget {
+  bool isSwitched;
+
+  IsAcceptingJoin(this.isSwitched);
+
+  @override
+  _IsAcceptingJoinState createState() => _IsAcceptingJoinState();
+}
+
+class _IsAcceptingJoinState extends State<IsAcceptingJoin> {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: false,
     );
   }
 }
