cb6e4d837e7fc4a78d1b2e38d6996cffd53a8431
HollandPleskac <hollandpleskac@gmail.com>
2020-07-06 14:36:49 -0700

    dynamic view students in class

--- lib/teacher_portal/view_class.dart
+++ lib/teacher_portal/view_class.dart
@@ -67,48 +67,15 @@ class _ViewClassState extends State<ViewClass> {
           Container(
             height: 360,
             child: _isTouchedAll == true
-                ? ListView(
-                    physics: BouncingScrollPhysics(),
-                    children: [
-                      Student(
-                        name: 'Holland Pleskac',
-                        status: 'Doing Great',
-                        profilePictureLink:
-                            'https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&q=70',
-                      ),
-                      Student(
-                        name: 'Shabd Veyyakula',
-                        status: 'Need Help',
-                        profilePictureLink:
-                            'https://img-cdn.tid.al/o/4858a4b2723b7d0c7d05584ff57701f7b0c54ce3.jpg',
-                      ),
-                      Student(
-                        name: 'Kushagra Singh',
-                        status: 'Inactive',
-                        profilePictureLink:
-                            'https://lh3.googleusercontent.com/proxy/s4loFI-1bt-dxkFqW8EnFXewl1hnBisUyUiQ_Mo6ucc8tSOSMvud02ylLQpj03vF8I_I9Qrb5qHCRyB20xrz0uy5_rE-1NuKdCWvEcHSo4jCB4guthqJqNnDIe-9K-5-zNamW3efFyfHuD-SQ6D_O8LFICvL9Lyi-g',
-                      ),
-                      Student(
-                        name: 'Pranav Krishna',
-                        status: 'Frustrated',
-                        profilePictureLink: null,
-                      ),
-                      Student(
-                        name: 'Advithi Kethidi',
-                        status: 'Inactive',
-                        profilePictureLink:
-                            'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-                      ),
-                    ],
-                  )
+                ? AllTab()
                 : _isTouchedDoingGreat
                     ? DoingGreatTab()
                     : _isTouchedNeedHelp
-                        ? Text('need help')
+                        ? NeedHelpTab()
                         : _isTouchedFrustrated
-                            ? Text('frustrated')
+                            ? FrustratedTab()
                             : _isTouchedInactive
-                                ? Text('inactive')
+                                ? InactiveTab()
                                 : Text('IMPORTANT - this text will never show since the first values will always be true'),
           ),
         ],
@@ -117,84 +84,7 @@ class _ViewClassState extends State<ViewClass> {
   }
 }
 
-class Student extends StatelessWidget {
-  final String name;
-  final String status;
-  final String profilePictureLink;
 
-  Student({this.name, this.status, this.profilePictureLink});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 80,
-      child: Row(
-        children: [
-          SizedBox(
-            width: 20,
-          ),
-          Container(
-            height: 60,
-            width: 60,
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              borderRadius: BorderRadius.circular(9999),
-            ),
-            child: profilePictureLink == null
-                ? Center(
-                    child: FaIcon(
-                      FontAwesomeIcons.userAlt,
-                      color: kPrimaryColor,
-                    ),
-                  )
-                : ClipRRect(
-                    borderRadius: BorderRadius.circular(9999),
-                    child: FittedBox(
-                      fit: BoxFit.cover,
-                      child: Image.network(
-                        profilePictureLink,
-                      ),
-                    ),
-                  ),
-          ),
-          SizedBox(
-            width: 20,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(),
-              Container(),
-              Text(
-                name,
-                style: TextStyle(fontSize: 16.5),
-              ),
-              Text(
-                'Last updated: 7 days ago',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.grey[600],
-                ),
-              ),
-              Container(),
-              Container(),
-            ],
-          ),
-          Spacer(),
-          FaIcon(
-            FontAwesomeIcons.solidComments,
-            color: kPrimaryColor,
-            size: 35,
-          ),
-          SizedBox(
-            width: 20,
-          )
-        ],
-      ),
-    );
-  }
-}
 
 ///
 ///                                            Filter Buttons
--- lib/widgets/filtered_tabs.dart
+++ lib/widgets/filtered_tabs.dart
@@ -1,10 +1,306 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../constant.dart';
+
+final Firestore _firestore = Firestore.instance;
+
+class AllTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _firestore
+          .collection('Classes')
+          .document('test class app ui')
+          .collection('Students')
+          
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return Center(
+              child: Container(),
+            );
+          default:
+            if (snapshot.data != null &&
+                snapshot.data.documents.isEmpty == false) {
+              return ListView(
+                physics: BouncingScrollPhysics(),
+                children:
+                    snapshot.data.documents.map((DocumentSnapshot document) {
+                  return Student(
+                    name: document['name'],
+                    status: document['status'],
+                    profilePictureLink: null,
+                  );
+                }).toList(),
+              );
+            } else {
+              return Center(
+                child: Text('no data'),
+              );
+            }
+        }
+      },
+    );
+  }
+}
 
 class DoingGreatTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _firestore
+          .collection('Classes')
+          .document('test class app ui')
+          .collection('Students')
+          .where("status", isEqualTo: 'doing great')
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return Center(
+              child: Container(),
+            );
+          default:
+            if (snapshot.data != null &&
+                snapshot.data.documents.isEmpty == false) {
+              return ListView(
+                physics: BouncingScrollPhysics(),
+                children:
+                    snapshot.data.documents.map((DocumentSnapshot document) {
+                  return Student(
+                    name: document['name'],
+                    status: document['status'],
+                    profilePictureLink: null,
+                  );
+                }).toList(),
+              );
+            } else {
+              return Center(
+                child: Text('no data'),
+              );
+            }
+        }
+      },
+    );
+  }
+}
+
+class NeedHelpTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _firestore
+          .collection('Classes')
+          .document('test class app ui')
+          .collection('Students')
+          .where("status", isEqualTo: 'need help')
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return Center(
+              child: Container(),
+            );
+          default:
+            if (snapshot.data != null &&
+                snapshot.data.documents.isEmpty == false) {
+              return ListView(
+                physics: BouncingScrollPhysics(),
+                children:
+                    snapshot.data.documents.map((DocumentSnapshot document) {
+                  return Student(
+                    name: document['name'],
+                    status: document['status'],
+                    profilePictureLink: null,
+                  );
+                }).toList(),
+              );
+            } else {
+              return Center(
+                child: Text('no data'),
+              );
+            }
+        }
+      },
+    );
+  }
+}
+
+class FrustratedTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _firestore
+          .collection('Classes')
+          .document('test class app ui')
+          .collection('Students')
+          .where("status", isEqualTo: 'frustrated')
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return Center(
+              child: Container(),
+            );
+          default:
+            if (snapshot.data != null &&
+                snapshot.data.documents.isEmpty == false) {
+              return ListView(
+                physics: BouncingScrollPhysics(),
+                children:
+                    snapshot.data.documents.map((DocumentSnapshot document) {
+                  return Student(
+                    name: document['name'],
+                    status: document['status'],
+                    profilePictureLink: null,
+                  );
+                }).toList(),
+              );
+            } else {
+              return Center(
+                child: Text('no data'),
+              );
+            }
+        }
+      },
+    );
+  }
+}
+
+class InactiveTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _firestore
+          .collection('Classes')
+          .document('test class app ui')
+          .collection('Students')
+          .where("status", isEqualTo: 'grey')
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return Center(
+              child: Container(),
+            );
+          default:
+            if (snapshot.data != null &&
+                snapshot.data.documents.isEmpty == false) {
+              return ListView(
+                physics: BouncingScrollPhysics(),
+                children:
+                    snapshot.data.documents.map((DocumentSnapshot document) {
+                  return Student(
+                    name: document['name'],
+                    status: document['status'],
+                    profilePictureLink: null,
+                  );
+                }).toList(),
+              );
+            } else {
+              return Center(
+                child: Text('no data'),
+              );
+            }
+        }
+      },
+    );
+  }
+}
+
+class Student extends StatelessWidget {
+  final String name;
+  final String status;
+  final String profilePictureLink;
+
+  Student({this.name, this.status, this.profilePictureLink});
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Text('viewing students doing great'),
+      width: MediaQuery.of(context).size.width,
+      height: 80,
+      child: Row(
+        children: [
+          SizedBox(
+            width: 20,
+          ),
+          Container(
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(9999),
+            ),
+            child: profilePictureLink == null
+                ? Center(
+                    child: FaIcon(
+                      FontAwesomeIcons.userAlt,
+                      color: kPrimaryColor,
+                    ),
+                  )
+                : ClipRRect(
+                    borderRadius: BorderRadius.circular(9999),
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: Image.network(
+                        profilePictureLink,
+                      ),
+                    ),
+                  ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(),
+              Container(),
+              Text(
+                name,
+                style: TextStyle(fontSize: 16.5),
+              ),
+              Text(
+                'Last updated: 7 days ago',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey[600],
+                ),
+              ),
+              Container(),
+              Container(),
+            ],
+          ),
+          Spacer(),
+          FaIcon(
+            FontAwesomeIcons.solidComments,
+            color: kPrimaryColor,
+            size: 35,
+          ),
+          SizedBox(
+            width: 20,
+          )
+        ],
+      ),
     );
   }
 }
