06db958704de3896565c40019bc44f04e55dba1a
HollandPleskac <hollandpleskac@gmail.com>
2020-07-13 16:49:37 -0700

    sign up logic

--- lib/logic/auth.dart
+++ lib/logic/auth.dart
@@ -16,18 +16,24 @@ class Auth {
       return ['success', email];
     } catch (error) {
       switch (error.code) {
+        case "ERROR_INVALID_EMAIL":
+          return ['failure', 'Email is badly formatted'];
+        case "ERROR_WRONG_PASSWORD":
+          return ['failure', 'Password is wrong'];
         case "ERROR_USER_NOT_FOUND":
-          return ['failure', 'No account exist with this email'];
+          return ['failure', 'No user exists for this email address'];
         case "ERROR_USER_DISABLED":
           return ['failure', 'Account has been disabled'];
-        case "ERROR_USER_TOKEN_EXPIRED":
-          return ['failure', 'User token expired'];
-        case "ERROR_EMAIL_ALREADY_IN_USE":
-          return ['failure', "Email already in use"];
-        case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
-          return ['failure', "Email already exists with different account"];
-        case "ERROR_CREDENTIAL_ALREADY_IN_USE":
-          return ['failure', "Credential already in use"];
+        case "ERROR_TOO_MANY_REQUESTS":
+          return [
+            'failure',
+            'Too many requests to sign in please wait to sign in'
+          ];
+        case "ERROR_OPERATION_NOT_ALLOWED":
+          return [
+            'failure',
+            'Account not enabled - contact {class vibes email}'
+          ];
       }
     }
     return ['success', email];
@@ -43,22 +49,46 @@ class Auth {
       return ['success', email];
     } catch (error) {
       switch (error.code) {
+        case "ERROR_INVALID_EMAIL":
+          return ['failure', 'Email is badly formatted'];
+        case "ERROR_WRONG_PASSWORD":
+          return ['failure', 'Password is wrong'];
         case "ERROR_USER_NOT_FOUND":
-          return ['failure', 'No account exist with this email'];
+          return ['failure', 'No user exists for this email address'];
         case "ERROR_USER_DISABLED":
           return ['failure', 'Account has been disabled'];
-        case "ERROR_USER_TOKEN_EXPIRED":
-          return ['failure', 'User token expired'];
-        case "ERROR_EMAIL_ALREADY_IN_USE":
-          return ['failure', "Email already in use"];
-        case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
-          return ['failure', "Email already exists with different account"];
-        case "ERROR_CREDENTIAL_ALREADY_IN_USE":
-          return ['failure', "Credential already in use"];
+        case "ERROR_TOO_MANY_REQUESTS":
+          return [
+            'failure',
+            'Too many requests to sign in please wait to sign in'
+          ];
+        case "ERROR_OPERATION_NOT_ALLOWED":
+          return [
+            'failure',
+            'Account not enabled - contact {class vibes email}'
+          ];
       }
     }
     return ['success', email];
   }
 
-  String signUp() {}
+  Future<List> signUp(String email, String password) async {
+    try {
+      AuthResult result = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+
+      return ['success', email];
+    } catch (error) {
+      switch (error.code) {
+        case "ERROR_WEAk_PASSWORD":
+          return ['failure', 'Password is not strong enough'];
+        case "ERROR_INVALID_EMAIL":
+          return ['failure', 'Email address formatted incorrectly'];
+        case "ERROR_EMAIL_ALREADY_IN_USE ":
+          return ['failure', 'Email already in use'];
+      }
+    }
+    return ['success', email];
+  }
 }
