ebacff96b728ecb5c58958ed35ec6292e82983fe
Kushagra Singh <56901151+KingRegera@users.noreply.github.com>
2020-07-12 21:33:35 -0700

    Add files via upload

---
+++ LoginViews/StudentLogin.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class StudentLogin extends StatefulWidget {
+  @override
+  _StudentLoginState createState() => _StudentLoginState();
+}
+
+class _StudentLoginState extends State<StudentLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.18,
+          ),
+          Center(
+            child: Text(
+              'Student',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 46,
+                  fontWeight: FontWeight.w300),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.15,
+          ),
+          Center(
+            child: Container(
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Email'),
+                  ),
+                ),
+              ),
+              height: MediaQuery.of(context).size.height * 0.06,
+              width: MediaQuery.of(context).size.width * 0.85,
+              decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.02,
+          ),
+          Center(
+            child: Container(
+              child: Center(
+                child:Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Password'),
+                  ),
+                ),
+              ),
+              height: MediaQuery.of(context).size.height * 0.06,
+              width: MediaQuery.of(context).size.width * 0.85,
+              decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.02,
+          ),
+          Center(
+            child: GestureDetector(
+              child: Container(
+                child: Center(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                        color: Colors.grey[100],
+                        fontSize: 24,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ),
+                height: MediaQuery.of(context).size.height * 0.06,
+                width: MediaQuery.of(context).size.width * 0.85,
+                decoration: BoxDecoration(
+                    color: Colors.blue[200],
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
