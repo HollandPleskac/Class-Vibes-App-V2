ec5534a52f18cd51f5b72355fbd6f4401710b3d5
HollandPleskac <hollandpleskac@gmail.com>
2020-07-13 14:08:09 -0700

    login teacher form

--- lib/auth/login_teacher.dart
+++ lib/auth/login_teacher.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../logic/auth.dart';
+import '../teacher_portal/classview_teacher.dart';
+
+final _auth = Auth();
+
 class TeacherLogin extends StatefulWidget {
   @override
   _TeacherLoginState createState() => _TeacherLoginState();
 }
 
 class _TeacherLoginState extends State<TeacherLogin> {
+  final _formKey = new GlobalKey<FormState>();
+  bool isEmailValidate = false;
+  bool isPasswordValidate = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,45 +35,73 @@ class _TeacherLoginState extends State<TeacherLogin> {
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.15,
           ),
-          Center(
-            child: Container(
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Email'),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Center(
+                  child: Container(
+                    child: Center(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none, hintText: 'Email'),
+                          validator: (value) {
+                            if (value == null || value == '') {
+                              setState(() {
+                                isEmailValidate = true;
+                              });
+
+                              return 'Email cannot be blank';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ),
+                    height: isEmailValidate == true
+                        ? MediaQuery.of(context).size.height * 0.08
+                        : MediaQuery.of(context).size.height * 0.06,
+                    width: MediaQuery.of(context).size.width * 0.85,
+                    decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: BorderRadius.circular(10)),
                   ),
                 ),
-              ),
-              height: MediaQuery.of(context).size.height * 0.06,
-              width: MediaQuery.of(context).size.width * 0.85,
-              decoration: BoxDecoration(
-                  color: Colors.grey[100],
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.02,
-          ),
-          Center(
-            child: Container(
-              child: Center(
-                child:Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Password'),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                Center(
+                  child: Container(
+                    child: Center(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none, hintText: 'Password'),
+                          validator: (value) {
+                            if (value == null || value == '') {
+                              setState(() {
+                                isPasswordValidate = true;
+                              });
+                              return 'Password cannot be blank';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ),
+                    height: isPasswordValidate == true
+                        ? MediaQuery.of(context).size.height * 0.08
+                        : MediaQuery.of(context).size.height * 0.06,
+                    width: MediaQuery.of(context).size.width * 0.85,
+                    decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: BorderRadius.circular(10)),
                   ),
                 ),
-              ),
-              height: MediaQuery.of(context).size.height * 0.06,
-              width: MediaQuery.of(context).size.width * 0.85,
-              decoration: BoxDecoration(
-                  color: Colors.grey[100],
-                  borderRadius: BorderRadius.circular(10)),
+              ],
             ),
           ),
           SizedBox(
@@ -73,6 +109,17 @@ class _TeacherLoginState extends State<TeacherLogin> {
           ),
           Center(
             child: GestureDetector(
+              onTap: () {
+                if (_formKey.currentState.validate()) {
+                  // _auth.teacherAsStudent();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ClassViewTeacher(),
+                    ),
+                  );
+                }
+              },
               child: Container(
                 child: Center(
                   child: Text(
@@ -86,8 +133,9 @@ class _TeacherLoginState extends State<TeacherLogin> {
                 height: MediaQuery.of(context).size.height * 0.06,
                 width: MediaQuery.of(context).size.width * 0.85,
                 decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(10)),
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
             ),
           )
