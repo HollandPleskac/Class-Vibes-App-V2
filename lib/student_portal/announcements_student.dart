0db378fa9bad7a195fdfe54f1b6d083d2317fcd6
HollandPleskac <hollandpleskac@gmail.com>
2020-07-14 12:01:31 -0700

    get announcements in a list firestore

--- lib/student_portal/announcements_student.dart
+++ lib/student_portal/announcements_student.dart
@@ -6,7 +6,49 @@ import '../nav_student.dart';
 
 final Firestore _firestore = Firestore.instance;
 
-class AnnouncementsStudent extends StatelessWidget {
+class AnnouncementsStudent extends StatefulWidget {
+  @override
+  _AnnouncementsStudentState createState() => _AnnouncementsStudentState();
+}
+
+class _AnnouncementsStudentState extends State<AnnouncementsStudent> {
+  List classIds = [];
+  List announcements = [];
+  Future getAnnouncements() async {
+    List<DocumentSnapshot> classDocuments = await _firestore
+        .collection('UserData')
+        .document('new@gmail.com')
+        .collection('Classes')
+        .getDocuments()
+        .then((querySnap) => querySnap.documents);
+
+    classDocuments.forEach((DocumentSnapshot document) {
+      classIds.add(document.documentID);
+    });
+
+    for (var i = 0; i < classIds.length; i++) {
+      List announcementDocuments = await _firestore
+          .collection('Classes')
+          .document(classIds[i])
+          .collection('Announcements')
+          .getDocuments()
+          .then((querySnap) => querySnap.documents);
+
+          announcementDocuments.forEach((document) { 
+            announcements.add(document.data);
+          });
+    }
+  }
+
+  @override
+  void initState() {
+    getAnnouncements().then((_) {
+      setState(() {});
+      print(announcements);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
