0ac037f1245d1b0a7aa8511b620087543e0c625e
HollandPleskac <hollandpleskac@gmail.com>
2020-07-15 11:02:56 -0700

    shared preferences import

--- lib/auth/login_teacher.dart
+++ lib/auth/login_teacher.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../logic/auth.dart';
 import '../teacher_portal/classview_teacher.dart';
