62420235445571af7a770caf9c493dce87d91976
HollandPleskac <hollandpleskac@gmail.com>
2020-07-15 14:37:42 -0700

    initialize join class screen

---
+++ lib/student_portal/join_class.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JoinClass extends StatefulWidget {
+  @override
+  _JoinClassState createState() => _JoinClassState();
+}
+
+class _JoinClassState extends State<JoinClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
