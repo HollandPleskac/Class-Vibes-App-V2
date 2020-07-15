53b702f4b6d098b58702a78a04dca5e7897123f7
HollandPleskac <hollandpleskac@gmail.com>
2020-07-15 14:46:06 -0700

    switch pin code fields

--- lib/student_portal/join_class.dart
+++ lib/student_portal/join_class.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+import '../constant.dart';
+import '../nav_student.dart';
+import '../logic/fire.dart';
 
 class JoinClass extends StatefulWidget {
   @override
@@ -9,7 +14,23 @@ class _JoinClassState extends State<JoinClass> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      drawer: NavStudent(),
+      appBar: AppBar(
+        title: Text('Join a Class'),
+        centerTitle: true,
+        backgroundColor: kWetAsphaltColor,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            PinCodeTextField(
+              
+              length: 3,
+              onChanged: null,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
