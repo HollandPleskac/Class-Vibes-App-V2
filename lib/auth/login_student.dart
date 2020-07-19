677a44793f4900d410e7b50ccd31488b49913741
HollandPleskac <hollandpleskac@gmail.com>
2020-07-19 12:52:18 -0700

    servers are down student log in

--- lib/auth/login_student.dart
+++ lib/auth/login_student.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../student_portal/classview_student.dart';
 import '../logic/auth.dart';
 
 final _auth = Auth();
+final Firestore _firestore = Firestore.instance;
 
 class StudentLogin extends StatefulWidget {
   @override
@@ -24,161 +25,195 @@ class _StudentLoginState extends State<StudentLogin> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ListView(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.18,
-          ),
-          Center(
-            child: Text(
-              'Student',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 46,
-                  fontWeight: FontWeight.w300),
-            ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.15,
-          ),
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Center(
-                  child: Container(
-                    child: Center(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*0.045),
-                        child: TextFormField(
-                          controller: _emailController,
-                          decoration: InputDecoration(
-                              border: InputBorder.none, hintText: 'Email'),
-                          validator: (value) {
-                            if (value == null || value == '') {
-                              setState(() {
-                                isEmailValidate = true;
-                              });
+      body: StreamBuilder(
+          stream: _firestore
+              .collection('Application Management')
+              .document('ServerManagement')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Text('');
+            } else {
+              return snapshot.data['serversAreUp'] == false
+                  ? Center(
+                      child: Text(
+                        'Servers are down',
+                        style: TextStyle(color: Colors.grey[800], fontSize: 18),
+                      ),
+                    )
+                  : ListView(
+                      children: [
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.18,
+                        ),
+                        Center(
+                          child: Text(
+                            'Student',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 46,
+                                fontWeight: FontWeight.w300),
+                          ),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.15,
+                        ),
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              Center(
+                                child: Container(
+                                  child: Center(
+                                    child: Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.045),
+                                      child: TextFormField(
+                                        controller: _emailController,
+                                        decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            hintText: 'Email'),
+                                        validator: (value) {
+                                          if (value == null || value == '') {
+                                            setState(() {
+                                              isEmailValidate = true;
+                                            });
 
-                              return 'Email cannot be blank';
-                            }
-                            return null;
-                          },
+                                            return 'Email cannot be blank';
+                                          }
+                                          return null;
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                  height: isEmailValidate == true
+                                      ? MediaQuery.of(context).size.height *
+                                          0.08
+                                      : MediaQuery.of(context).size.height *
+                                          0.06,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.85,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey[100],
+                                      borderRadius: BorderRadius.circular(10)),
+                                ),
+                              ),
+                              SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.02,
+                              ),
+                              Center(
+                                child: Container(
+                                  child: Center(
+                                    child: Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.045),
+                                      child: TextFormField(
+                                        controller: _passwordController,
+                                        decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            hintText: 'Password'),
+                                        validator: (value) {
+                                          if (value == null || value == '') {
+                                            setState(() {
+                                              isPasswordValidate = true;
+                                            });
+                                            return 'Password cannot be blank';
+                                          }
+                                          return null;
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                  height: isPasswordValidate == true
+                                      ? MediaQuery.of(context).size.height *
+                                          0.08
+                                      : MediaQuery.of(context).size.height *
+                                          0.06,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.85,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey[100],
+                                      borderRadius: BorderRadius.circular(10)),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ),
-                    height: isEmailValidate == true
-                        ? MediaQuery.of(context).size.height * 0.08
-                        : MediaQuery.of(context).size.height * 0.06,
-                    width: MediaQuery.of(context).size.width * 0.85,
-                    decoration: BoxDecoration(
-                        color: Colors.grey[100],
-                        borderRadius: BorderRadius.circular(10)),
-                  ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
-                Center(
-                  child: Container(
-                    child: Center(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*0.045),
-                        child: TextFormField(
-                          controller: _passwordController,
-                          decoration: InputDecoration(
-                              border: InputBorder.none, hintText: 'Password'),
-                          validator: (value) {
-                            if (value == null || value == '') {
-                              setState(() {
-                                isPasswordValidate = true;
-                              });
-                              return 'Password cannot be blank';
-                            }
-                            return null;
-                          },
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
                         ),
-                      ),
-                    ),
-                    height: isPasswordValidate == true
-                        ? MediaQuery.of(context).size.height * 0.08
-                        : MediaQuery.of(context).size.height * 0.06,
-                    width: MediaQuery.of(context).size.width * 0.85,
-                    decoration: BoxDecoration(
-                        color: Colors.grey[100],
-                        borderRadius: BorderRadius.circular(10)),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.02,
-          ),
-          Center(
-            child: GestureDetector(
-              onTap: () async {
-                if (_formKey.currentState.validate()) {
-                  List result = await _auth.loginAsStudent(
-                      email: _emailController.text,
-                      password: _passwordController.text);
-                  if (result[0] == 'success') {
-                
-                    //push next screen
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ClassViewStudent(),
-                      ),
+                        Center(
+                          child: GestureDetector(
+                            onTap: () async {
+                              if (_formKey.currentState.validate()) {
+                                List result = await _auth.loginAsStudent(
+                                    email: _emailController.text,
+                                    password: _passwordController.text);
+                                if (result[0] == 'success') {
+                                  //push next screen
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => ClassViewStudent(),
+                                    ),
+                                  );
+                                } else {
+                                  setState(() {
+                                    isEmailValidate = false;
+                                    isPasswordValidate = false;
+                                    _feedback = result[1];
+                                  });
+                                }
+                              } else {
+                                //determines if you need a big textfield box to display a validator message
+                                setState(() {
+                                  isEmailValidate == true
+                                      ? isEmailValidate = true
+                                      : isEmailValidate = false;
+                                  isPasswordValidate == true
+                                      ? isPasswordValidate = true
+                                      : isPasswordValidate = false;
+                                });
+                              }
+                            },
+                            child: Container(
+                              child: Center(
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(
+                                      color: Colors.grey[100],
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                              height: MediaQuery.of(context).size.height * 0.06,
+                              width: MediaQuery.of(context).size.width * 0.85,
+                              decoration: BoxDecoration(
+                                  color: Colors.blue[200],
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Center(
+                          child: Text(
+                            _feedback,
+                            style: TextStyle(color: Colors.red, fontSize: 15.5),
+                          ),
+                        ),
+                      ],
                     );
-                  } else {
-                    setState(() {
-                      isEmailValidate = false;
-                      isPasswordValidate = false;
-                      _feedback = result[1];
-                    });
-                  }
-                } else {
-                  //determines if you need a big textfield box to display a validator message
-                  setState(() {
-                    isEmailValidate == true
-                        ? isEmailValidate = true
-                        : isEmailValidate = false;
-                    isPasswordValidate == true
-                        ? isPasswordValidate = true
-                        : isPasswordValidate = false;
-                  });
-                }
-              },
-              child: Container(
-                child: Center(
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                        color: Colors.grey[100],
-                        fontSize: 24,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ),
-                height: MediaQuery.of(context).size.height * 0.06,
-                width: MediaQuery.of(context).size.width * 0.85,
-                decoration: BoxDecoration(
-                    color: Colors.blue[200],
-                    borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Center(
-            child: Text(
-              _feedback,
-              style: TextStyle(color: Colors.red, fontSize: 15.5),
-            ),
-          ),
-        ],
-      ),
+            }
+          }),
     );
   }
 }
