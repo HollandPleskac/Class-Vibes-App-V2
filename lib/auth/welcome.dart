d9299a30a02e54411f22478d345e2f4a1b90fc50
HollandPleskac <hollandpleskac@gmail.com>
2020-07-19 12:50:54 -0700

    servers are down on welcome screen

--- lib/auth/welcome.dart
+++ lib/auth/welcome.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'login_student.dart';
 import 'login_teacher.dart';
 import 'sign_up.dart';
+
+final Firestore _firestore = Firestore.instance;
 
 class Welcome extends StatefulWidget {
   @override
@@ -14,109 +17,130 @@ class _WelcomeState extends State<Welcome> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.25,
-          ),
-          Center(
-            child: Text(
-              'Login',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 46,
-                  fontWeight: FontWeight.w300),
-            ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.15,
-          ),
-          Center(
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => StudentLogin()),
-                );
-              },
-              child: Container(
-                child: Center(
-                  child: Text(
-                    'Student',
-                    style: TextStyle(
-                        color: Colors.grey[600],
-                        fontSize: 24,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ),
-                height: MediaQuery.of(context).size.height * 0.06,
-                width: MediaQuery.of(context).size.width * 0.85,
-                decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.02,
-          ),
-          Center(
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => TeacherLogin()),
-                );
-              },
-              child: Container(
-                child: Center(
-                  child: Text(
-                    'Teacher',
-                    style: TextStyle(
-                        color: Colors.grey[600],
-                        fontSize: 24,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ),
-                height: MediaQuery.of(context).size.height * 0.06,
-                width: MediaQuery.of(context).size.width * 0.85,
-                decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.02,
-          ),
-          Center(
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignUp()),
-                );
-              },
-              child: Container(
-                child: Center(
-                  child: Text(
-                    'Register',
-                    style: TextStyle(
-                        color: Colors.grey[600],
-                        fontSize: 24,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ),
-                height: MediaQuery.of(context).size.height * 0.06,
-                width: MediaQuery.of(context).size.width * 0.85,
-                decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-          )
-        ],
-      ),
+      body: StreamBuilder(
+          stream: _firestore
+              .collection('Application Management')
+              .document('ServerManagement')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Text('');
+            } else {
+              return snapshot.data['serversAreUp'] == false
+                  ? Center(
+                      child: Text(
+                        'Servers are down',
+                        style: TextStyle(color: Colors.grey[800], fontSize: 18),
+                      ),
+                    )
+                  : Column(
+                      children: [
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.25,
+                        ),
+                        Center(
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 46,
+                                fontWeight: FontWeight.w300),
+                          ),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.15,
+                        ),
+                        Center(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => StudentLogin()),
+                              );
+                            },
+                            child: Container(
+                              child: Center(
+                                child: Text(
+                                  'Student',
+                                  style: TextStyle(
+                                      color: Colors.grey[600],
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                              height: MediaQuery.of(context).size.height * 0.06,
+                              width: MediaQuery.of(context).size.width * 0.85,
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[100],
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        Center(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => TeacherLogin()),
+                              );
+                            },
+                            child: Container(
+                              child: Center(
+                                child: Text(
+                                  'Teacher',
+                                  style: TextStyle(
+                                      color: Colors.grey[600],
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                              height: MediaQuery.of(context).size.height * 0.06,
+                              width: MediaQuery.of(context).size.width * 0.85,
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[100],
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        Center(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SignUp()),
+                              );
+                            },
+                            child: Container(
+                              child: Center(
+                                child: Text(
+                                  'Register',
+                                  style: TextStyle(
+                                      color: Colors.grey[600],
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                              height: MediaQuery.of(context).size.height * 0.06,
+                              width: MediaQuery.of(context).size.width * 0.85,
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[100],
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                          ),
+                        )
+                      ],
+                    );
+            }
+          }),
     );
   }
 }
