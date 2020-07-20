b3db5142ccf7c3e2312a97e45c9cbfc115a4cfdf
HollandPleskac <hollandpleskac@gmail.com>
2020-07-19 23:13:14 -0700

    servers down styling

--- lib/widgets/server_down.dart
+++ lib/widgets/server_down.dart
@@ -4,7 +4,7 @@ class ServersDown extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(250, 250, 250, 1),
+      backgroundColor: Color.fromRGBO(235, 235, 235, 1),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -14,22 +14,27 @@ class ServersDown extends StatelessWidget {
               'assets/images/serverdown-isometric.png',
               width: MediaQuery.of(context).size.width * 0.8,
             ),
-          
             Text(
               'Servers are Down',
-              style: TextStyle(color: Colors.grey[600], fontSize: 42),
+              style: TextStyle(
+                color: Colors.grey[600],
+                fontSize: 42,
+                letterSpacing: 1.2,
+                fontWeight: FontWeight.w400
+              ),
             ),
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.0275,
             ),
             Padding(
-              padding: EdgeInsets.only(left:20,right:20),
-                          child: Text(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Text(
                 'Sorry for the inconvience, but our servers are down for maintenance. Please check back later. This page will allow you to get back to what you were doing when servers are up.',
                 textAlign: TextAlign.center,
-
                 style: TextStyle(
-                  fontSize: 15.5,
+                  fontSize: 18,
+                  color: Colors.grey[600],
+                  fontWeight: FontWeight.w400,
                 ),
               ),
             ),
