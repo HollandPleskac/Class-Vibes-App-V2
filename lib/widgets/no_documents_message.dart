53b9704f8ac8f4ec09eec2661e02f9441e916be7
HollandPleskac <hollandpleskac@gmail.com>
2020-07-19 23:21:13 -0700

    no docs message widgets

---
+++ lib/widgets/no_documents_message.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoDocsClassViewStudent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
