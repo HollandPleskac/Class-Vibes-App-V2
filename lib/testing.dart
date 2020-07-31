27dcc0ce7931969b6baca9157d18e542ff598d29
HollandPleskac <hollandpleskac@gmail.com>
2020-07-31 12:01:07 -0700

    comments

--- lib/testing.dart
+++ lib/testing.dart
@@ -1,28 +1,28 @@
-import 'package:googleapis/calendar/v3.dart';
-import 'package:googleapis_auth/auth_io.dart';
+// import 'package:googleapis/calendar/v3.dart';
+// import 'package:googleapis_auth/auth_io.dart';
 
-//get Service Account Credentials
-final accountCredentials = new ServiceAccountCredentials.fromJson({
-  "private_key_id": "myprivatekeyid",
-  "private_key": "myprivatekey",
-  "client_email": "myclientemail",
-  "client_id": "myclientid",
-  "type": "service_account"
-});
-var _scopes = [
-  CalendarApi.CalendarScope
-]; //defines the scopes for the calendar api
+// //get Service Account Credentials
+// final accountCredentials = new ServiceAccountCredentials.fromJson({
+//   "private_key_id": "myprivatekeyid",
+//   "private_key": "myprivatekey",
+//   "client_email": "myclientemail",
+//   "client_id": "myclientid",
+//   "type": "service_account"
+// });
+// var _scopes = [
+//   CalendarApi.CalendarScope
+// ]; //defines the scopes for the calendar api
 
-void getCalendarEvents() {
-  clientViaServiceAccount(accountCredentials, _scopes).then((client) {
-    var calendar = new CalendarApi(client);
-    var calEvents = calendar.events.list("primary");
-    calEvents.then((Events events) {
-      events.items.forEach(
-        (Event event) {
-          print(event.summary);
-        },
-      );
-    });
-  });
-}
+// void getCalendarEvents() {
+//   clientViaServiceAccount(accountCredentials, _scopes).then((client) {
+//     var calendar = new CalendarApi(client);
+//     var calEvents = calendar.events.list("primary");
+//     calEvents.then((Events events) {
+//       events.items.forEach(
+//         (Event event) {
+//           print(event.summary);
+//         },
+//       );
+//     });
+//   });
+// }
