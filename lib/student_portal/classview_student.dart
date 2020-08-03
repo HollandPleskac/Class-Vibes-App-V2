7e923f5383b44b55f6ba72581939d3845e6296b7
HollandPleskac <hollandpleskac@gmail.com>
2020-08-03 13:59:25 -0700

    bug

--- lib/student_portal/classview_student.dart
+++ lib/student_portal/classview_student.dart
@@ -260,7 +260,7 @@ class StudentClass extends StatelessWidget {
               if (!snapshot.hasData) {
                 return Text('');
               } else {
-                return UnreadMessageBadge(snapshot.data['teacher unread']);;
+                return UnreadMessageBadge(snapshot.data['student unread']);
               }
             },
           ),
