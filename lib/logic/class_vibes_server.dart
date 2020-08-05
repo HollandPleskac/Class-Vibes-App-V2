a6c20745c26f86913ec17ba9515902aff2f9b3ae
HollandPleskac <hollandpleskac@gmail.com>
2020-08-05 14:52:02 -0700

    verify sending announcements with auth token

--- lib/logic/class_vibes_server.dart
+++ lib/logic/class_vibes_server.dart
@@ -1,4 +1,7 @@
 import 'package:http/http.dart' as http;
+import 'package:firebase_auth/firebase_auth.dart';
+
+final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
 class ClassVibesServer {
   Future<void> sendEmailForAnnouncement(
@@ -9,8 +12,10 @@ class ClassVibesServer {
   ) async {
     print(className);
     print(classCode);
+    final FirebaseUser user = await _firebaseAuth.currentUser();
+    final IdTokenResult token = await user.getIdToken();
     var getResponse = await http.get(
-        'http://api.classvibes.net/api/sendEmail?code=$classCode&title=$title&message=$message&className=$className');
+        'http://api.classvibes.net/api/sendEmail?code=$classCode&title=$title&message=$message&className=$className&apiToken=$token');
     print(getResponse.body);
   }
 }
