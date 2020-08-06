d63764160460d1a5d4d3a1eff11026334dc0f953
HollandPleskac <hollandpleskac@gmail.com>
2020-08-05 17:20:12 -0700

    remove server token

--- lib/logic/fcm.dart
+++ lib/logic/fcm.dart
@@ -4,8 +4,9 @@ import 'dart:async';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:http/http.dart' as http;
 
-final String serverToken =
-    'AAAA2miS9yY:APA91bEU74Nt7Ddt4HGq14lDMvh0Gar7vsc2SfiqTQWhS01xkQpF6QmALEYM6c-4CqD4RFdJFTIubtUZvPmZP4fwP3vvUbyIrXdbrpUYXMNWmVeq6r9lzYt2DfgLqYRsNdxKjuXnLrEl';
+//DELETE THIS DONT HAVE THIS IN CLIENT VERSION OF APP
+// final String serverToken =
+//     'secret token not safe in client';
 
 final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
