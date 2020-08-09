b3da8eb5fc779d9badb90cf7e5006b865d68feeb
HollandPleskac <hollandpleskac@gmail.com>
2020-08-09 14:55:24 -0700

    changes?

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 
 // Colors
 
-
 // const kPrimaryColor = Color.fromRGBO(9, 132, 227,1.0); //Electron Blue
 const kPrimaryColor = Color.fromRGBO(55, 93, 206, 1); // Web Color
 
 const kWetAsphaltColor = Color.fromRGBO(52, 73, 94,1.0);
-// const kWetAsphaltColor = Color.fromRGBO(55, 93, 206, 1);
+// const kWetAsphaltColor = Color.fromRGBO(78, 114, 222, 1);
 const kPieChartDoingGreatColor = Color(0xff13d38e);
 const kPieChartNeedHelpColor = Color(0xfff8b250);
 const kPieChartFrustratedColor = Colors.red;
