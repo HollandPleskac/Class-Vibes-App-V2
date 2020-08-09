6ef348cc3193794f4be177290cc067c77b1a0a79
HollandPleskac <hollandpleskac@gmail.com>
2020-08-09 15:06:40 -0700

    change app bar colors

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 // Colors
 
 // const kPrimaryColor = Color.fromRGBO(9, 132, 227,1.0); //Electron Blue
-const kPrimaryColor = Color.fromRGBO(55, 93, 206, 1); // Web Color
+const kPrimaryColor = Color.fromRGBO(55, 93, 206, 1);
 const kAppBarColor = Color.fromRGBO(78, 114, 222, 1);
-
 const kWetAsphaltColor = Color.fromRGBO(52, 73, 94,1.0);
-// const kWetAsphaltColor = Color.fromRGBO(78, 114, 222, 1);
+
 const kPieChartDoingGreatColor = Color(0xff13d38e);
 const kPieChartNeedHelpColor = Color(0xfff8b250);
 const kPieChartFrustratedColor = Colors.red;
