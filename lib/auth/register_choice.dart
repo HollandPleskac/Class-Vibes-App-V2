8cf4fe17c8dc7d023959ae34e912fac9a4771688
HollandPleskac <hollandpleskac@gmail.com>
2020-08-10 15:02:45 -0700

    back arrows

--- lib/auth/register_choice.dart
+++ lib/auth/register_choice.dart
@@ -11,6 +11,18 @@ class _RegisterChoiceState extends State<RegisterChoice> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.grey,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+      ),
       body: ListView(
         children: [
           SizedBox(
--- lib/auth/student_sign_up.dart
+++ lib/auth/student_sign_up.dart
@@ -4,7 +4,18 @@ class SignUpStudent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.grey,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+      ),
     );
   }
 }
