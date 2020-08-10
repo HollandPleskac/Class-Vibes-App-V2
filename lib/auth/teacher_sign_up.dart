fb88a723018891e006446e8e00f2fe75e08cb296
HollandPleskac <hollandpleskac@gmail.com>
2020-08-10 15:01:48 -0700

    routing

--- lib/auth/teacher_sign_up.dart
+++ lib/auth/teacher_sign_up.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class SignUpTeacher extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
       
     );
   }
