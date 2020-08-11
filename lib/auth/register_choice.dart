03ceda4d20735ba481da70317d62b2c8bf4475f3
Kushagra Singh <kushthaksh@gmail.com>
2020-08-11 15:15:03 -0700

    Ripple Effect & announcement light theme

--- lib/auth/register_choice.dart
+++ lib/auth/register_choice.dart
@@ -28,58 +28,83 @@ class _RegisterChoiceState extends State<RegisterChoice> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.blue[300],
-                  borderRadius: BorderRadius.circular(10)),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SignUpStudent()));
-                },
-                child: Container(
-                  child: Center(
-                    child: Text(
-                      'Student',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.w300),
+                      child: Text(
+                        'Register As',
+                        style: TextStyle(
+                            color: Colors.grey[800],
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
                     ),
+                    SizedBox(height: 50,),
+          Center(
+            child: Container(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SignUpStudent()));
+                  },
+                  child: Container(
+                    child: Center(
+                      child: Text(
+                        'Student',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                    height: MediaQuery.of(context).size.width * 0.6,
+                    width: MediaQuery.of(context).size.width * 0.9,
                   ),
-                  height: MediaQuery.of(context).size.width * 0.6,
-                  width: MediaQuery.of(context).size.width * 0.9,
                 ),
               ),
+              
+                  decoration: BoxDecoration(
+                      color: Colors.blue[300],
+                      borderRadius: BorderRadius.circular(10)),
+               height: MediaQuery.of(context).size.width * 0.6,
+                  width: MediaQuery.of(context).size.width * 0.9,
             ),
           ),
           SizedBox(
             height: 20,
           ),
           Center(
-            child: InkWell(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SignUpTeacher()));
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.deepOrange[400],
-                    borderRadius: BorderRadius.circular(10)),
-                child: Center(
-                  child: Text(
-                    'Teacher',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.w300),
+            child: Container(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SignUpTeacher()));
+                  },
+                  child: Container(
+                    child: Center(
+                      child: Text(
+                        'Teacher',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                    height: MediaQuery.of(context).size.width * 0.6,
+                    width: MediaQuery.of(context).size.width * 0.9,
                   ),
                 ),
-                height: MediaQuery.of(context).size.width * 0.6,
-                width: MediaQuery.of(context).size.width * 0.9,
               ),
+              
+                  decoration: BoxDecoration(
+                      color: Colors.deepOrange[400],
+                      borderRadius: BorderRadius.circular(10)),
+               height: MediaQuery.of(context).size.width * 0.6,
+                  width: MediaQuery.of(context).size.width * 0.9,
             ),
           ),
+          SizedBox(height: 50,),
         ],
       ),
     );
