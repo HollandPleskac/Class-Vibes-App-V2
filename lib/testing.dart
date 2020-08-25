6d673ba129b0a0492162e521eb5f0f2e9d980d33
HollandPleskac <hollandpleskac@gmail.com>
2020-08-24 19:44:57 -0700

    ??

--- lib/testing.dart
+++ lib/testing.dart
@@ -1,31 +1,14 @@
-// import 'package:class_vibes_v2/auth/welcome.dart';
-// import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
 
-// import './logic/updates.dart';
 
-// final Firestore _firestore = Firestore.instance;
-
-// final updates = Updates();
-
-// class DeactivatedAccountScreen extends StatefulWidget {
-//   final String teacherEmail;
-//   DeactivatedAccountScreen({this.teacherEmail});
-//   @override
-//   _DeactivatedAccountScreenState createState() =>
-//       _DeactivatedAccountScreenState();
-// }
-
-// class _DeactivatedAccountScreenState extends State<DeactivatedAccountScreen> {
-  
-//   @override
-//   void initState() {
-//     updates.handleAccountStatus(context, widget.teacherEmail);
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold();
-//   }
-// }
+class Testing extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(),
+      ),
+    );
+  }
+}
