d9b13818461a3f66079cc7165b00909dd7a20ef1
HollandPleskac <hollandpleskac@gmail.com>
2020-09-21 15:29:11 -0700

    help screen

--- lib/teacher_portal/help_settings.dart
+++ lib/teacher_portal/help_settings.dart
@@ -1,4 +1,5 @@
 import 'package:class_vibes_v2/auth/welcome.dart';
+import 'package:class_vibes_v2/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -16,30 +17,38 @@ class HelpScreen extends StatelessWidget {
   final String email;
   final String accountType;
 
-  HelpScreen(this.email,this.accountType);
+  HelpScreen(this.email, this.accountType);
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Container(
-        child: FlatButton(
-          child: Text('Delete forever'),
-          onPressed: () async {
-            FirebaseUser user = await _firebaseAuth.currentUser();
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Contact support@classvibes.net with any questions!',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: kPrimaryColor,fontSize: 15,fontWeight: FontWeight.w500),
+          ),
+          FlatButton(
+            child: Text('Delete Account'),
+            onPressed: () async {
+              FirebaseUser user = await _firebaseAuth.currentUser();
 
-            print('deleteing + ' + accountType);
+              print('deleteing + ' + accountType);
 
-            await _classVibesServer.deleteAccount(
-                email: email, accountType: accountType);
+              await _classVibesServer.deleteAccount(
+                  email: email, accountType: accountType);
 
-            // only signs out google auth because otherwise there is no "user" to delete
-            await _auth.signOutGoogle();
-            await user.delete();
+              // only signs out google auth because otherwise there is no "user" to delete
+              await _auth.signOutGoogle();
+              await user.delete();
 
-            Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(builder: (context) => Welcome()),
-                (Route<dynamic> route) => false);
-          },
-        ),
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) => Welcome()),
+                  (Route<dynamic> route) => false);
+            },
+          ),
+        ],
       ),
     );
   }
