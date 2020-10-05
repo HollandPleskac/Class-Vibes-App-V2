3e927807735d73c307e08d14a3b380bf5212a432
HollandPleskac <hollandpleskac@gmail.com>
2020-10-04 20:03:45 -0700

    router in main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:class_vibes_v2/student_portal/classview_student.dart';
+import 'package:class_vibes_v2/teacher_portal/classview_teacher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import './student_portal/chat_student.dart';
 import 'teacher_portal/view_class.dart';
 import 'teacher_portal/class_settings.dart';
 import './teacher_portal/chat_teacher.dart';
 import './student_portal/view_class_student.dart';
-import './router.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import './logic/auth.dart';
 
-// void main() => runApp((MyApp()));
+final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+final _auth = Auth();
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,16 +25,83 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+  String type;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  // check if the user is a teacher or student
+  void getAccountType() async {
+    User user = _firebaseAuth.currentUser;
+    try {
+      String accountType = await _auth.checkAccountType(user.email);
+
+      setState(() {
+        if (accountType == 'Student') {
+          type = 'student';
+        } else {
+          type = 'teacher';
+        }
+      });
+    } catch (e) {
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    getAccountType();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Show error message if initialization failed
+    if (_error) {
+      return SomethingWentWrong();
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return Loading();
+    }
+
+    return MyAwesomeApp(type);
+  }
+}
+
+class MyAwesomeApp extends StatelessWidget {
+  final String type;
+
+  MyAwesomeApp(this.type);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RouterScreen(),
+      home: type == 'student' ? ClassViewStudent() : ClassViewTeacher(),
       routes: {
         ViewClass.routeName: (context) => ViewClass(),
         ClassSettings.routeName: (context) => ClassSettings(),
@@ -37,6 +109,28 @@ class _MyAppState extends State<MyApp> {
         ChatTeacher.routeName: (context) => ChatTeacher(),
         ViewClassStudent.routename: (context) => ViewClassStudent(),
       },
+    );
+  }
+}
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
+
+class SomethingWentWrong extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Something went wrong, please restart the app'),
+      ),
     );
   }
 }
