a9caeacf565dea6420fa6c45e0a8d0a5357f52cf
HollandPleskac <hollandpleskac@gmail.com>
2020-10-09 16:12:45 -0700

    teacher authentication sign in working

--- lib/logic/auth_service.dart
+++ lib/logic/auth_service.dart
@@ -145,6 +145,108 @@ class AuthenticationService {
 
   // Teacher Email
 
+  Future<String> signInEmailTeacher(String email, String password) async {
+    try {
+      final UserCredential cred = await _firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      if (await _fire.getAccountType(email) != 'Teacher') {
+        await _firebaseAuth.signOut();
+        return 'Account exists as a student. Please use the student sign in';
+      }
+
+      if (!cred.user.emailVerified) {
+        await cred.user.sendEmailVerification();
+        await _firebaseAuth.signOut();
+        return 'Verify your email to continue. We sent you a verification email.';
+      }
+
+      await _revenueCat.signInRevenueCat(cred.user.uid);
+
+      return "Signed in";
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'invalid-email':
+          return 'The email address is not valid';
+        case 'user-disabled':
+          return 'The user cooresponding to this email address has been disabled';
+        case 'user-not-found':
+          return 'No user exists with this email address. Please sign up first.';
+        case 'wrong-password':
+          return 'The password is incorrect';
+        default:
+          return 'An unknown error occurred';
+      }
+    } catch (e) {
+      print(e);
+      return 'An unknown error occurred';
+    }
+  }
+
   // Teacher Google
+
+  Future<String> signInGoogleTeacher() async {
+   
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    print(googleUser.email);
+
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    try {
+      final User user =
+          (await _firebaseAuth.signInWithCredential(credential)).user;
+
+      if (await _fire.isUserInDb(user.email) == false) {
+        await signoutFirebase();
+        return 'No user exists with this email address. Please sign up first.';
+      }
+
+      if (await _fire.getAccountType(user.email) != 'Teacher') {
+        await signoutFirebase();
+        return 'Account exists as a student. Please use the student sign in';
+      }
+
+      await _revenueCat.signInRevenueCat(user.uid);
+      return 'Signed in';
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'account-exists-with-different-credential':
+          return 'Account exists with a different credential';
+
+        case 'invalid-credential':
+          return 'The credential is invalid';
+
+        case 'operation-not-allowed':
+          return 'Operation not allowed. Your account is not enabled';
+
+        case 'user-disabled':
+          return 'The user cooresponding to this email address has been disabled';
+
+        case 'user-not-found':
+          return 'No user exists with this email address. Please sign up first.';
+
+        case 'wrong-password':
+          return 'The password is incorrect';
+
+        case 'invalid-verification-code':
+          return 'The verification code is incorrect';
+
+        case 'invalid-verification-id':
+          return 'The verification id is incorrect';
+
+        default:
+          return 'An unknown error occurred';
+      }
+    } catch (e) {
+      print(e);
+      return 'An unknown error occurred';
+    }
+  }
 
 }
